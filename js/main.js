2092960205636d65e3bd124b645a0cdd50c1ed49
Deepesh Makhijani <deepeshmakhijani@live.com>
2016-02-15 00:17:13 +0530

    The last one

--- js/main.js
+++ js/main.js
@@ -80,11 +80,20 @@ TweenMax.to(".star12", 2, {y:700, yoyo:true, repeat:10, rotation:720, ease: Powe
 */
 
 
-TweenMax.to(document.getElementById("balloon1"), 2.5 + 0.5 * Math.random(), {y:-1200, repeat:10, repeatDelay:0.5, ease:Quad.easeIn});
-TweenMax.to(document.getElementById("balloon2"), 2.5 + 0.5 * Math.random(), {y:-1200, repeat:10, repeatDelay:0.5, ease:Quad.easeIn});
-TweenMax.to(document.getElementById("balloon3"), 2.5 + 0.5 * Math.random(), {y:-1200, repeat:10, repeatDelay:0.5, ease:Quad.easeIn});
-TweenMax.to(document.getElementById("balloon4"), 2.5 + 0.5 * Math.random(), {y:-1200, repeat:10, repeatDelay:0.5, ease:Quad.easeIn});
+TweenMax.to(document.getElementById("balloon1"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+TweenMax.to(document.getElementById("balloon2"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+TweenMax.to(document.getElementById("balloon3"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+TweenMax.to(document.getElementById("balloon4"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
 
+setTimeout(doSomething, 1000);
+
+function doSomething() {
+   TweenMax.to(document.getElementById("balloon5"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+TweenMax.to(document.getElementById("balloon6"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+TweenMax.to(document.getElementById("balloon7"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+TweenMax.to(document.getElementById("balloon8"), 3.5 + 1.5 * Math.random(), {y:-1200, repeat:25, repeatDelay:0.5, ease: Power0.easeNone});
+
+}
 
 
 
